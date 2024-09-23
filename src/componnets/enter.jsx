76ffa692de3e7a51d2a8c157e44b4f47ead09d82
IZUMI-GIT <133763814+IZUMI-GIT@users.noreply.github.com>
2024-09-23 23:06:25 +0530

    axios.get with params

--- src/componnets/enter.jsx
+++ src/componnets/enter.jsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from "react"
 import axios from "axios";
 export default function Enter({onSend}) {
 
-    const [city, setCity] = useState("vizag");
+    const [city, setCity] = useState("lucknow");
 
     useEffect(()=>{
         handleClick();
     },[])
 
     const handleClick = () => {
-            axios.get(`http://api.weatherapi.com/v1/current.json?key=228eddb43e554cb0a6831049242309&q=${city}&aqi=no`)
+            axios({
+               method : "get",
+               url : `http://api.weatherapi.com/v1/current.json`,
+               params: {
+               key : "228eddb43e554cb0a6831049242309",
+               q :  city,
+               aqi :"no"
+                    },
+                })
                 .then((response) => onSend(response.data))
                 .catch((error) => {
                     console.error("Error fetching weather data:", error)
